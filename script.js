5a18950588f978fa2f69714131d92294a2b9b181
Ayo <ayobamiajeleke@gmail.com>
2023-07-07 14:06:07 +0100

    Hamburger Toggle

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const hamburger=document.querySelector('#hamburg')
 const nav=document.querySelector('.nav2')
+const close=document.querySelector('#close-menu')
 
 hamburger.addEventListener('click', ()=> {
+    hamburger.classList.add('hide');
+    close.classList.remove('hide')
+    nav.classList.toggle('visible')
+})
+
+
+close.addEventListener('click', ()=> {
+    hamburger.classList.remove('hide');
+    close.classList.add('hide')
     nav.classList.toggle('visible')
 })
